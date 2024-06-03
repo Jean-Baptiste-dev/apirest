3ce59d4b5ce8b59ad27da7a5cad78f9cc84c169a
Jean-Baptiste-Dev <jeankougbenou@gmail.com>
2024-06-04 00:02:39 +0200

    add some api file

---
+++ src/Controller/BookController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class BookController extends AbstractController
+{
+
+    public function __construct(
+        private BookRepository $bookrepo,
+        private SerializerInterface $serializer
+    ) {
+    }
+    #[Route('/api/books', name: 'book', methods: ['GET'])]
+    public function getAllBooks(): JsonResponse
+    {
+        $books = $this->bookrepo->findAll();
+        $jsonBookList = $this->serializer->serialize($books, 'json', ['groups' => 'getBooks']);
+        return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/books/{id}', name: 'detailBook', methods: ['GET'])]
+    public function getDetailBook(Book $book, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonBook = $serializer->serialize($book, 'json', ['groups' => 'getBooks']);
+        return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
+    }
+}
---
+++ src/Controller/PostController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+
+    public function __construct(private PostRepository $postRepository, private EntityManagerInterface $em)
+    {
+    }
+    #[Route('/post', name: 'app.post', methods: ['GET'])]
+    public function index(): Response
+    {
+        $posts =  $this->postRepository->findAll();
+        return $this->render('post/index.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+    #[Route('/post/create', name: 'app.post.create', methods: ['POST'])]
+    public function create(Request $request)
+    {
+        $post = new Post;
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($post);
+            $this->em->flush();
+
+            return $this->render('post/index.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }
+    }
+    #[Route('/post/edit/{id}', name: 'app.post.edit', methods: ['PUT', 'PATCH'])]
+    public function edit(Request $request, Post $post)
+    {
+
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //$this->em->persist($post);
+            $this->em->flush();
+            return $this->render('post/index.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }
+    }
+
+    #[Route('/post/delete/{id}', name: 'app.post.delete')]
+    public function delete(Post $post)
+    {
+        $this->em->remove($post);
+        $this->em->flush();
+        $this->redirectToRoute('app.post');
+    }
+}
